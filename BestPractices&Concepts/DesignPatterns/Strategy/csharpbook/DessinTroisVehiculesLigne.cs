78d9dce819ade71e07aa4d8d3eae2aa0e94bf0dd
Yoel PEPIN <yoel.pepin@non.se.com>
2024-04-30 19:05:38 +0200

    Pattern Strategy : CsharpBook

---
+++ BestPractices&Concepts/DesignPatterns/Strategy/csharpbook/DessinTroisVehiculesLigne.cs
@@ -0,0 +1,28 @@
+﻿namespace BestPractices_Concepts.DesignPatterns.Strategy.csharpbook;
+
+public class DessinTroisVehiculesLigne : DessinCatalogue
+{
+    public void dessine(IList<VueVehicule> contenu)
+    {
+        int compteur = 0;
+        Console.WriteLine("Dessine les véhicules avec 3 véhicules par ligne :");
+        foreach (VueVehicule vue in contenu)
+        {
+            vue.dessine();
+            compteur++;
+            if (compteur == 3)
+            {
+                compteur = 0;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        if (compteur != 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BestPractices&Concepts/DesignPatterns/Strategy/csharpbook/UtilisateurStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace BestPractices_Concepts.DesignPatterns.Strategy.csharpbook;
+
+public class UtilisateurStrategy
+{
+    public static void Main()
+    {
+        VueCatalogue vueCatalogue1 = new VueCatalogue(new DessinTroisVehiculesLigne());
+        vueCatalogue1.dessine();
+        VueCatalogue vueCatalogue2 = new VueCatalogue(new DessinVehiculeLigne());
+        vueCatalogue2.dessine();
+    }
+}
