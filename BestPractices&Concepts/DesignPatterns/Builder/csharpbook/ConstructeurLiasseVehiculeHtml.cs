f502c624da382af319bd1acbf6e38990bf6f13eb
Yoel PEPIN <yoel.pepin@non.se.com>
2024-04-30 18:44:53 +0200

    CSharpBook ; Builder

---
+++ BestPractices&Concepts/DesignPatterns/Builder/csharpbook/ConstructeurLiasseVehiculeHtml.cs
@@ -0,0 +1,23 @@
+﻿namespace BestPractices_Concepts.DesignPatterns.Builder.csharpbook;
+
+public class ConstructeurLiasseVehiculeHtml : ConstructeurLiasseVehicule
+{
+    public ConstructeurLiasseVehiculeHtml()
+    {
+        liasse = new LiasseHtml();
+    }
+
+    public override void ConstruitBonDeCommande(string nomClient)
+    {
+        string document;
+        document = "<HTML>Bon de commande Client : " + nomClient + "</HTML>";
+        liasse.ajouteDocument(document);
+    }
+
+    public override void ConstruitDemandeImmatriculation(string nomDemandeur)
+    {
+        string document;
+        document = "<HTML>Demande d'immatriculation Demandeur : " + nomDemandeur + "</HTML>";
+        liasse.ajouteDocument(document);
+    }
+}
